5e42fd22719871b6fad8dd60003e8ec241316832
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-09-30 19:44:46 +0800

    refactor 買一本書的情況

--- TDD_BuyHarryPotter/main.cs
+++ TDD_BuyHarryPotter/main.cs
@@ -8,11 +8,12 @@ namespace TDD_BuyHarryPotter
 {
     public class BookStore
     {
+        private const int _bookPrice = 100;
+
         static void Main() { }
         public int BuyBook(Dictionary<int, int> books)
         {
-            return 100;
-            throw new NotImplementedException();
+            return books[1] * _bookPrice;            
         }
     }
 }
