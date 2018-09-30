ddd8b567997012aa27a6f0e0364d60a96f23f316
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-09-30 17:31:27 +0800

    UnitTest For Buy 2 type

--- TDD_BuyHarryPotter/UnitTest1.cs
+++ TDD_BuyHarryPotter/UnitTest1.cs
@@ -18,5 +18,16 @@ namespace TDD_BuyHarryPotter
 
             Assert.AreEqual(expected, buyBook);
         }
+
+        [TestMethod]
+        public void BuyHarryPotter_Buy2TypeBook_ShouldBe195()
+        {
+            var bookStore = new BookStore();
+            int buyBook = bookStore.BuyBook(2);
+
+            var expected = 195;
+
+            Assert.AreEqual(expected, buyBook);
+        }
     }
 }
