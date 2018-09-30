35836fdf3de87074c45b3917f85f443d5fb86e30
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-09-30 19:52:08 +0800

    買兩本不同種類的情況

--- TDD_BuyHarryPotter/main.cs
+++ TDD_BuyHarryPotter/main.cs
@@ -13,7 +13,12 @@ namespace TDD_BuyHarryPotter
         static void Main() { }
         public int BuyBook(Dictionary<int, int> books)
         {
-            return books[1] * _bookPrice;            
+            if (books.Count() == 2)
+            {
+                return (int)(books[1] * _bookPrice * 0.95 + books[2] * _bookPrice * 0.95);
+            }
+
+            return books[1] * _bookPrice;
         }
     }
 }
