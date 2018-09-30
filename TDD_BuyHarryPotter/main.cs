d23ed065333efe6938516555b1dfd94d328e53ae
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-09-30 19:54:57 +0800

    refactor 合併計算兩本不同種類的情況

--- TDD_BuyHarryPotter/main.cs
+++ TDD_BuyHarryPotter/main.cs
@@ -9,13 +9,15 @@ namespace TDD_BuyHarryPotter
     public class BookStore
     {
         private const int _bookPrice = 100;
+        private double _discount = 0.95;
 
         static void Main() { }
         public int BuyBook(Dictionary<int, int> books)
         {
             if (books.Count() == 2)
             {
-                return (int)(books[1] * _bookPrice * 0.95 + books[2] * _bookPrice * 0.95);
+                var bookSum = books.Sum(r => r.Value);
+                return (int)(bookSum * _bookPrice * _discount);
             }
 
             return books[1] * _bookPrice;
