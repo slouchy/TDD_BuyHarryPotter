9cd906679d904a7a8c97e8a7d54ccb468a143e90
Po-CHEN LEE <asd06a@yahoo.com.tw>
2018-09-30 17:26:03 +0800

    Init

---
+++ TDD_BuyHarryPotter/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TDD_BuyHarryPotter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+    }
+}
---
+++ TDD_BuyHarryPotter/main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_BuyHarryPotter
+{
+    public class BookStore
+    {
+        static void Main() { }
+        internal int BuyBook(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
